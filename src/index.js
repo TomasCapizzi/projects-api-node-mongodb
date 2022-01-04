3913d69a75483b32b039d0eac1d8f9ed993e0422
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-04 12:08:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+//Mongoose
 const mongoose = require('mongoose');
+//Controller
 const Projects = require('./api/projects');
+
+
+//Variables de entorno traidas por config
 const config = require('./config');
+
+//Cors
 const cors = require('cors');
 
 const app = express(); 
@@ -12,12 +19,15 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('public'))
 
 //Route + Controller
 app.use('/api/projects', Projects)
 
+
+//Conexión a MongoDB
 mongoose.connect(
-    config.db_uri,
+    config.db_uri, //"mongodb://localhost/projects"
     {
         useUnifiedTopology: true,
         useNewUrlParser: true
